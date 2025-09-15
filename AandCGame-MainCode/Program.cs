26733b07351c5a714ad00295530821b5af22475a
Camilla <camilla.n94@gmail.com>
2025-09-15 11:17:10 +0200

    Velkommen

--- AandCGame-MainCode/Program.cs
+++ AandCGame-MainCode/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AAndCGamesMainCode
+namespace AandCGame_MainCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Velkommen");
         }
     }
 }
