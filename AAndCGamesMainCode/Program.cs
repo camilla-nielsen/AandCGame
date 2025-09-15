58186560cae51a2027aacbba006ffc70c2bf61ff
Camilla <camilla.n94@gmail.com>
2025-09-15 12:20:06 +0200

    Menuen opsat

--- AAndCGamesMainCode/Program.cs
+++ AAndCGamesMainCode/Program.cs
@@ -12,6 +12,30 @@ namespace AAndCGamesMainCode
         {
             Console.WriteLine("Velkommen til");
             Console.WriteLine("A & C Games");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Vælg mellem disse spil:");
+            Console.WriteLine("1. Hangman");
+            Console.WriteLine("2. Sten, saks og papir");
+            Console.WriteLine();
+            Console.WriteLine("Tryk 1 for at spille Hangman og 2 for at spille sten, saks og papir.");
+
+            string valg = Console.ReadLine();
+            
+
+            if (valg == "1")
+            {
+                Console.WriteLine("Hangman");
+            }
+            
+            if (valg == "2")
+            {
+                Console.WriteLine("Sten, saks og papir");
+            }
+            else
+            {
+                Console.WriteLine("Tryk 1 for at spille Hangman og 2 for at spille sten, saks og papir.");
+            }
         }
     }
 }
